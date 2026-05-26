bc1049ee6ca2517eeb1559f12be2d242c1408628
Farshad Naderi <152512060+Drshahw@users.noreply.github.com>
2026-05-26 17:28:13 +0400

    feat: add public API exports

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+export type {
+  AionActor,
+  AionActorRole,
+  AionDiagnostic,
+  AionEntity,
+  AionField,
+  AionIO,
+  AionMetadata,
+  AionOperation,
+  AionProgram,
+  AionScalarType,
+  AionValidationResult,
+} from "./ir/types.js";
+
+export { AionParseError, parseAionProgram } from "./parser/parseAionProgram.js";
+export { validateAionProgram } from "./validator/validateAionProgram.js";
+export { compileAionProgramToPlan } from "./compiler/compileAionProgramToPlan.js";
+export type { AionCompilePlan, AionCompileStep } from "./compiler/compileAionProgramToPlan.js";
+export { createRuntimeManifest } from "./runtime/createRuntimeManifest.js";
+export type { AionRuntimeManifest } from "./runtime/createRuntimeManifest.js";
