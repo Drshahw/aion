a71268a1394ed96e7ef0cc3b03ed63ebf00a8d81
Farshad Naderi <152512060+Drshahw@users.noreply.github.com>
2026-05-26 18:13:50 +0400

    feat: export SQL compiler target

--- src/index.ts
+++ src/index.ts
@@ -18,5 +18,7 @@ export { compileAionProgramToPlan } from "./compiler/compileAionProgramToPlan.js
 export type { AionCompilePlan, AionCompileStep } from "./compiler/compileAionProgramToPlan.js";
 export { compileToTypeScript } from "./compiler/targets/typescript/compileToTypeScript.js";
 export type { TypeScriptCompileOptions } from "./compiler/targets/typescript/compileToTypeScript.js";
+export { compileToSql } from "./compiler/targets/sql/compileToSql.js";
+export type { SqlCompileOptions } from "./compiler/targets/sql/compileToSql.js";
 export { createRuntimeManifest } from "./runtime/createRuntimeManifest.js";
 export type { AionRuntimeManifest } from "./runtime/createRuntimeManifest.js";
